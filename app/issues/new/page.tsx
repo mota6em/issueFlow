b3f2faf08525497721727ef373260a3391db7883
motasem abubaraka <mota6em@gmail.com>
2025-03-15 05:26:31 +0100

    Enhance issue creation page with Markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,26 +1,22 @@
 "use client";
 import React from "react";
-
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 const page = () => {
   return (
-    <div className="w-6/12 space-y-3">
+    <div className="w-6/12 space-y-2">
       <h1 className="text-2xl py-2 font-bold">New Issue</h1>
       <legend className="fieldset-legend text-lg">Title</legend>
       <input
         type="text"
         placeholder="Please enter the title of the issue"
-        className="input input-primary w-full focus:border-black/0"
+        className="input input-primary w-full border-white/85 focus:outline-white focus:border-white/0"
       />
-      <fieldset className="fieldset">
-        <legend className="fieldset-legend text-lg">Description</legend>
-        <input
-          type="text"
-          className="input input-primary py-10 w-full focus:border-black/0"
-          placeholder="Please describe the issue"
-        />
-        {/* <p className="fieldset-label">Optional</p> */}
-      </fieldset>
-      <button className="btn btn-primary mt-3 hover:border-white">Submit New Issue</button>
+      <p className="text-lg">Description</p>
+      <SimpleMDE className="" placeholder="Please describe the issue" />
+      <button className="btn btn-primary mt-1 hover:border-white">
+        Submit New Issue
+      </button>
     </div>
   );
 };
