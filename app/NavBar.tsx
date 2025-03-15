dfdd4827441e46cd8c233063720e1f11a05fbbdd
motasem abubaraka <mota6em@gmail.com>
2025-03-15 01:17:58 +0100

    Fix theme toggle logic

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoBugOutline } from "react-icons/io5";
 
 const NavBar = () => {
@@ -9,6 +9,10 @@ const NavBar = () => {
     { href: "/", label: "Issues" },
   ];
   const [theme, setTheme] = useState("dark");
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <div className="flex items-center justify-between space-x-8 h-12 border-b px-10">
       <div className="flex items-center space-x-10">
@@ -28,14 +32,29 @@ const NavBar = () => {
           ))}
         </ul>
       </div>
-      <button
-        className="flex items-center"
-        onClick={() => {
-          setTheme(theme === "dark" ? "light" : "dark");
-          document.documentElement.setAttribute("data-theme", theme);
-        }}
-      >
+      <button className="flex items-center">
         <label className="flex cursor-pointer gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>{" "}
+          <input
+            type="checkbox"
+            value="synthwave"
+            className="toggle theme-controller"
+            onChange={() => {
+              setTheme(theme === "dark" ? "light" : "dark");
+            }}
+          />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
@@ -49,24 +68,6 @@ const NavBar = () => {
           >
             <circle cx="12" cy="12" r="5" />
             <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-          </svg>
-          <input
-            type="checkbox"
-            value="synthwave"
-            className="toggle theme-controller"
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
           </svg>
         </label>
       </button>
