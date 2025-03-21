5f549f1f56b064ca9aaa39135538d61a1588875e
motasem abubaraka <mota6em@gmail.com>
2025-03-21 17:07:20 +0100

    Implement authentication in NavBar and add AuthProvider for session management

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,7 +4,9 @@ import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { IoBugOutline } from "react-icons/io5";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
 const NavBar = () => {
+  const { status, data: session } = useSession();
   const Links = [
     { href: "/", label: "Dashboard" },
     { href: "/issues", label: "Issues" },
@@ -37,45 +39,55 @@ const NavBar = () => {
           ))}
         </ul>
       </div>
-      <button className="flex items-center">
-        <label className="flex cursor-pointer gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-          </svg>{" "}
-          <input
-            type="checkbox"
-            value="synthwave"
-            className="toggle theme-controller"
-            onChange={() => {
-              setTheme(theme === "dark" ? "light" : "dark");
-            }}
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <circle cx="12" cy="12" r="5" />
-            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-          </svg>
-        </label>
-      </button>
+      <div className="flex items-center space-x-5">
+        <div className="">
+          {status === "unauthenticated" && (
+            <Link href="/api/auth/signin">Login</Link>
+          )}
+          {status === "authenticated" && (
+            <Link href="/api/auth/signout">Logout</Link>
+          )}
+        </div>
+        <button className="flex items-center">
+          <label className="flex cursor-pointer gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>{" "}
+            <input
+              type="checkbox"
+              value="synthwave"
+              className="toggle theme-controller"
+              onChange={() => {
+                setTheme(theme === "dark" ? "light" : "dark");
+              }}
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="5" />
+              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+            </svg>
+          </label>
+        </button>
+      </div>
     </div>
   );
 };
