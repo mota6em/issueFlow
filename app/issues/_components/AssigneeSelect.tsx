9102b4fcecab7151f8504c7e5c401e645b4ea2ac
Motasem AbuBaraka <mota6em@gmail.com>
2025-03-23 22:04:53 +0100

    Refactor AssigneeSelect component to use custom hook for user fetching and streamline user assignment logic

--- app/issues/_components/AssigneeSelect.tsx
+++ app/issues/_components/AssigneeSelect.tsx
@@ -5,21 +5,9 @@ import toast, { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React from "react";
-import { MdOutlineReportGmailerrorred } from "react-icons/md";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: async () => {
-      return await axios("/api/users").then((res) => res.data);
-    },
-    staleTime: 60 * 1000,
-    retry: 13,
-  });
+  const { data: users, error, isLoading } = useUsers();
   if (isLoading) return <Skeleton className="w-48 h-10" />;
   if (error) return null;
   return (
@@ -27,25 +15,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
       <select
         onChange={(e) => {
           const userID = e.target.value;
-          console.log(userID);
-          const updateAssignedUser = async () => {
-            await axios
-              .patch(`/api/issues/${issue.id}`, {
-                assignedToUserId: userID || null,
-              })
-              .then(() => {
-                const assignedUser = users?.find((user) => user.id === userID);
-                toast.success(
-                  "Issue assigned to " + (assignedUser?.name || "No user")  
-                );
-              })
-              .catch(() =>
-                toast.error(
-                  "An unexpected error occurred. Please try again later."
-                )
-              );
-          };
-          updateAssignedUser();
+          updateAssignedUser(issue, userID, users || []);
         }}
         defaultValue={issue.assignedToUserId || ""}
         className="select select-primary cursor-pointer w-48 py-2  "
@@ -65,3 +35,31 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 };
 
 export default AssigneeSelect;
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: async () => {
+      return await axios("/api/users").then((res) => res.data);
+    },
+    staleTime: 60 * 1000,
+    retry: 13,
+  });
+
+const updateAssignedUser = async (
+  issue: Issue,
+  userID: string,
+  users: User[]
+) => {
+  await axios
+    .patch(`/api/issues/${issue.id}`, {
+      assignedToUserId: userID || null,
+    })
+    .then(() => {
+      const assignedUser = users?.find((user) => user.id === userID);
+      toast.success("Issue assigned to " + (assignedUser?.name || "No user"));
+    })
+    .catch(() =>
+      toast.error("An unexpected error occurred. Please try again later.")
+    );
+};
