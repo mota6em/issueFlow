04610dd84b57f9b40aec7324ac685d9f4fe87cfd
Motasem AbuBaraka <mota6em@gmail.com>
2025-03-23 14:24:51 +0100

    Fetch users from API in AssigneeSelect component and handle loading/error states

--- app/issues/_components/AssigneeSelect.tsx
+++ app/issues/_components/AssigneeSelect.tsx
@@ -1,13 +1,36 @@
 "use client";
+import { Skeleton } from "@/app/components";
+import { User } from "@prisma/client";
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 
 const AssigneeSelect = () => {
-  const users = ["User 1", "User 2", "User 3", "User 4"];
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: async () => {
+      return await axios("/api/users").then((res) => res.data);
+    },
+    staleTime: 60 * 1000,
+    retry: 13,
+  });
+  if (isLoading) return <Skeleton className="w-48 h-10" />;
+  if (error) return null;
   return (
-    <select defaultValue="Assing to user" className="select select-primary ">
+    <select
+      defaultValue="Assing to user"
+      className="select select-primary cursor-pointer w-48"
+    >
       <option disabled={true}>Assing to user</option>
-      {users.map((user) => (
-        <option key={user}>{user}</option>
+      {users?.map((user) => (
+        <option className="text-base mx-5" key={user.id}>
+          {user.name}
+        </option>
       ))}
     </select>
   );
