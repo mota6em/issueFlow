df54bb7bb8ad263187184dfc7999bbaa8bf45d53
Motasem AbuBaraka <mota6em@gmail.com>
2025-03-24 17:14:17 +0100

    Add IssueChart component to visualize issue statuses and integrate it into the Home page

---
+++ app/IssueChart.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar } from "recharts";
+interface Props {
+  openIssues: number;
+  inProgressIssues: number;
+  closedIssues: number;
+}
+const IssueChart = ({ openIssues, inProgressIssues, closedIssues }: Props) => {
+  const data = [
+    {
+      label: "Open Issues",
+      value: openIssues,
+    },
+    {
+      label: "In Progress Issues",
+      value: inProgressIssues,
+    },
+    {
+      label: "Closed Issues",
+      value: closedIssues,
+    },
+  ];
+  return (
+    <div className="card bg-base-100 w-lg shadow-sm">
+      <div className="card-body">
+        <div className="card-title">Issue Status Summary</div>
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart data={data}>
+            <XAxis
+              dataKey="label"
+              stroke="#8884d8"
+              tick={{ fill: "var(--color-base-content)", fontSize: 14 }}
+            />
+            <YAxis
+              stroke="#8884d8"
+              tick={{ fill: "var(--color-base-content)", fontSize: 14 }}
+            />
+            <Bar
+              dataKey="value"
+              barSize={60}
+              style={{ fill: "var(--color-primary)" }}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
+
+export default IssueChart;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
 export default function Home() {
   return (
     <div className="w-full flex items-center justify-between ">
-      <IssueSummary openIssues={3} inProgressIssues={34} closedIssues={2} />
-      <LatestIssues />
+      {/* <IssueSummary openIssues={3} inProgressIssues={34} closedIssues={2} />
+      <LatestIssues /> */}
+      <IssueChart openIssues={3} inProgressIssues={34} closedIssues={2} />
     </div>
   );
 }
